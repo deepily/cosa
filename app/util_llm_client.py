df58c093b524121cec456997bb01143a1b81ea62
deepily <42214371+deepily@users.noreply.github.com>
2025-01-15 21:20:36 -0500

    And debugging flags and output

--- app/util_llm_client.py
+++ app/util_llm_client.py
@@ -1,13 +1,17 @@
 import re
 
+import cosa.utils.util as du
 from cosa.utils.util_stopwatch import Stopwatch
 
-def query_llm_in_memory( model, tokenizer, prompt, device="cuda:0", model_name="ACME LLMs, Inc.", max_new_tokens=128, silent=False ):
+def query_llm_in_memory( model, tokenizer, prompt, device="cuda:0", model_name="ACME LLMs, Inc.", max_new_tokens=128, silent=False, debug=False, verbose=False ):
 
     timer = Stopwatch( msg=f"Asking LLM [{model_name}]...".format( model_name ), silent=silent )
     
     inputs        = tokenizer( prompt, return_tensors="pt" ).to( device )
     stop_token_id = tokenizer.encode( "</response>" )[ 0 ]
+    
+    du.print_banner( "Stop Token IDs " )
+    print( stop_token_id )
     
     generation_output = model.generate(
         input_ids=inputs[ "input_ids" ],
@@ -25,6 +29,10 @@ def query_llm_in_memory( model, tokenizer, prompt, device="cuda:0", model_name="
     seconds = timer.get_delta_ms() / 1000.0
     tokens_per_second = len( generation_output[ 0 ][ input_length: ] ) / seconds
     print( f"Tokens per second [{round( tokens_per_second, 1 )}] input tokens [{input_length}] + xml response tokens [{len( generation_output[ 0 ][ input_length: ] )}] = total tokens i/o [{len( generation_output[ 0 ] )}]" )
+    
+    if debug and verbose:
+        du.print_banner( "Raw response" )
+        print( raw_response )
     
     # Remove the <s> and </s> tags
     response = raw_response.replace( "</s><s>", "" ).strip()
