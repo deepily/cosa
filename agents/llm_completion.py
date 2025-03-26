b471aa56338cd1b1a21124eb44bb01db7824a418
deepily <42214371+deepily@users.noreply.github.com>
2025-03-25 22:40:53 -0400

    Remove dead + tweak

--- agents/llm_completion.py
+++ agents/llm_completion.py
@@ -1,14 +1,14 @@
 import requests
 import json
 
-from huggingface_hub import InferenceClient
-
 import cosa.utils.util as du
 from cosa.utils.util_stopwatch import Stopwatch
 
-import openai
-
-def get_completion_by_requests( prompt, url ):
+def get_completion(
+    prompt,
+    url="http://192.168.1.21:3000/v1/completions",
+    model="/mnt/DATA01/include/www.deepily.ai/projects/models/Ministral-8B-Instruct-2410.lora/merged-on-2025-02-12-at-02-05/autoround-4-bits-sym.gptq/2025-02-12-at-02-27"
+):
     
     headers = {
         'Content-Type' : 'application/json',
@@ -17,7 +17,7 @@ def get_completion_by_requests( prompt, url ):
     
     # Request a completion
     data = {
-        "model": "/mnt/DATA01/include/www.deepily.ai/projects/models/Ministral-8B-Instruct-2410/merged-00-2025-01-09.gptq",
+        "model": model,
         "prompt": prompt,
         "max_tokens": 32,
         "temperature": 0.25,
@@ -28,65 +28,20 @@ def get_completion_by_requests( prompt, url ):
     
     if response.status_code == 200:
         completion = response.json()
-        return completion[ 'choices' ][ 0 ][ 'text' ]
+        return completion[ 'choices' ][ 0 ][ 'text' ].replace( "```", "" ).strip()
     else:
         print( f"Error: {response.status_code}" )
         raise Exception( f"Error requesting completion: {response.text}" )
     
-def get_completion_by_openai( prompt, api_key ):
-    
-    return None
-
-# def get_completion_by_hfc( prompt, url, model_name="ACME LLMs, Inc.", max_new_tokens=1024, temperature=0.25, top_k=10,top_p=0.9, silent=False, debug=False, verbose=False ):
-#
-#     timer = Stopwatch( msg=f"Asking LLM [{model_name}]...".format( model_name ), silent=silent )
-#
-#     client = InferenceClient( url )
-#     token_list = [ ]
-#     ellipsis_count = 0
-#
-#     # if self.debug and self.verbose:
-#     #     for line in prompt.split( "\n" ):
-#     #         print( line )
-#
-#     for token in client.text_generation(
-#             prompt, max_new_tokens=max_new_tokens, stream=True, temperature=temperature, top_k=top_k, top_p=top_p,
-#             stop_sequences=[ "</response>" ]
-#     ):
-#         if debug:
-#             print( token, end="" )
-#         else:
-#             if not silent: print( ".", end="" )
-#             ellipsis_count += 1
-#             if ellipsis_count == 120:
-#                 ellipsis_count = 0
-#                 print()
-#
-#         token_list.append( token )
-#
-#     response = "".join( token_list ).strip()
-#
-#     timer.print( msg="Done!", use_millis=True, prepend_nl=True, end="\n" )
-#     tokens_per_second = len( token_list ) / (timer.get_delta_ms() / 1000.0)
-#     print( f"Tokens per second [{round( tokens_per_second, 1 )}]" )
-#
-#     if debug:
-#         print( f"Token list length [{len( token_list )}]" )
-#         if verbose:
-#             for line in response.split( "\n" ):
-#                 print( line )
-#
-#     return response
 
 if __name__ == "__main__":
     
-    url = "http://192.168.1.21:3000/v1/completions"
+    # url = "http://192.168.1.21:3000/v1/completions"
     
-    prompt_template = du.get_file_as_string( du.get_project_root() + "/src/conf/prompts/vox-command-template.txt" )
+    prompt_template = du.get_file_as_string( du.get_project_root() + "/src/conf/prompts/vox-command-template-completion-mistral-8b.txt" )
     voice_command = "I want you to open a new tab and do a Google scholar search on agentic behaviors"
     prompt = prompt_template.format( voice_command=voice_command )
     print( prompt )
     
-    response = get_completion_by_requests( prompt, url )
-    # response = get_completion_by_hfc( prompt, url )
+    response = get_completion( prompt )
     print( response )
